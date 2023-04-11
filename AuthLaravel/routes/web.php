f5c256bb5c5d1139aa76f9ad89d2ee08d15b261a
Antoine Boucher <antoine.boucher012@gmail.com>
2023-04-11 13:26:57 -0400

    update

--- AuthLaravel/routes/web.php
+++ AuthLaravel/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +32,7 @@ Route::get('/dashboard', function () {
 
 Route::get('/admin_dashboard', function () {
     return Inertia::render('AdminDashboard');
-})->middleware(['auth', 'admin'])->name('admin_dashboard');
+})->middleware(['auth', 'hasrole:admin'])->name('admin_dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -45,9 +46,77 @@ Route::group(['middleware' => ['auth', 'user'], 'prefix' => 'user'], function ()
 });
 
 // admin protected routes
-Route::group(['middleware' => ['auth', 'admin'], 'prefix' => 'admin'], function () {
+Route::group(['middleware' => ['auth', 'hasrole:admin'], 'prefix' => 'admin'], function () {
     Route::get('/', 'HomeController@index')->name('admin_dashboard');
     Route::get('/users', 'AdminUserController@list')->name('admin_users');
 });
+
+// Route::get('/forgot-password', function () {
+//     return Inertia::render('Auth/ForgotPassword');
+// })->middleware('guest')->name('password.request');
+
+// Route::post('/forgot-password', function (Request $request) {
+//     $request->validate(['email' => 'required|email']);
+
+//     $status = Password::sendResetLink(
+//         $request->only('email')
+//     );
+
+//     return $status === Password::RESET_LINK_SENT
+//                 ? back()->with(['status' => __($status)])
+//                 : back()->withErrors(['email' => __($status)]);
+// })->middleware('guest')->name('password.email');
+
+
+// Route::get('/auth/redirect', function () {
+//     return Socialite::driver('github')->redirect();
+// });
+
+// Route::get('/auth/callback', function () {
+//     $githubUser = Socialite::driver('github')->user();
+
+//     $user = User::updateOrCreate([
+//         'github_id' => $githubUser->id,
+//     ], [
+//         'name' => $githubUser->name,
+//         'email' => $githubUser->email,
+//         'github_token' => $githubUser->token,
+//         'github_refresh_token' => $githubUser->refreshToken,
+//     ]);
+
+//     Auth::login($user);
+
+//     return redirect('/dashboard');
+// });
+
+// Route::get('/reset-password/{token}', function (string $token) {
+//     return view('auth.reset-password', ['token' => $token]);
+// })->middleware('guest')->name('password.reset');
+
+// Route::post('/reset-password', function (Request $request) {
+//     $request->validate([
+//         'token' => 'required',
+//         'email' => 'required|email',
+//         'password' => 'required|min:8|confirmed',
+//     ]);
+
+//     $status = Password::reset(
+//         $request->only('email', 'password', 'password_confirmation', 'token'),
+//         function (User $user, string $password) {
+//             $user->forceFill([
+//                 'password' => Hash::make($password)
+//             ])->setRememberToken(Str::random(60));
+
+//             $user->save();
+
+//             event(new PasswordReset($user));
+//         }
+//     );
+
+//     return $status === Password::PASSWORD_RESET
+//                 ? redirect()->route('login')->with('status', __($status))
+//                 : back()->withErrors(['email' => [__($status)]]);
+// })->middleware('guest')->name('password.update');
+
 
 require __DIR__.'/auth.php';
